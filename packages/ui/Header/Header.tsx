f574bdac1d203e06be91c2b0fd0179c560d2538d
Lewis Flude <lewis@lewisflude.com>
2022-11-08 16:04:19 +0000

    Use package for tailwind-config

--- packages/ui/Header/Header.tsx
+++ packages/ui/Header/Header.tsx
@@ -1,13 +1,12 @@
 import { ReactNode } from "react";
 import Image from "next/image";
-import styles from "./Header.module.css";
 
 export interface HeaderProps {
   children?: ReactNode;
 }
 export function Header({ children }: HeaderProps) {
   return (
-    <header className={styles.header}>
+    <header>
       <Image src="/logo.png" width={80} height={80} alt="Logo" />
       {children}
     </header>

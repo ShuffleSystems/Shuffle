a10d40103ea414735f3978eed40aa3d35ced6496
Lewis Flude <lewis@lewisflude.com>
2022-11-08 00:56:44 +0000

    Fix issue preventing turbopack from being usable with turborepo

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,18 +9,4 @@ module.exports = {
     "turbo",
     "prettier",
   ],
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-
-      parserOptions: {
-        project: ["./tsconfig.json"],
-      },
-    },
-  ],
 };
